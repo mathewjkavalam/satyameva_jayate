bb68b2c6df0ebb61217869125040ef227c8854a5
Mathew Jacob Kavalam <mathewjkavalam@gmail.com>
2025-02-22 17:33:11 +0000

    add animal image uploader

--- Python Code/Data_Entry_UI_and_Vizualization/ui_data_entry.py
+++ Python Code/Data_Entry_UI_and_Vizualization/ui_data_entry.py
@@ -1,3 +1,4 @@
+# TODO: ask if each input should have separate submit buttons
 '''salve, regina, mater misericordiae...
 Take care of data entry
 example:
@@ -10,3 +11,12 @@ import streamlit as ui
 # TODO: connect with DB to save the input for later
 animal_classification = ui.text_input(
     label="Animal classification", value="Panthera tigris tigris")
+
+# TODO: ask if multiple file upload is priority
+# store the uploaded image locally, so that it can be previewed
+animal_image = ui.file_uploader(
+    label="Choose the image of the animal", accept_multiple_files=False)
+if animal_image is None:
+    pass
+else:
+  ui.image(image= animal_image)
