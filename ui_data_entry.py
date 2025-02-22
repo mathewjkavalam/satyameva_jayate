cf6edf7371d193d1e71fa89aafef46d756cd25f8
Mathew Jacob Kavalam <mathewjkavalam@gmail.com>
2025-02-22 16:01:16 +0000

    add initial design comments

---
+++ ui_data_entry.py
@@ -0,0 +1,7 @@
+'''salve, regina, mater misericordiae...
+Take care of data entry
+example:
+>>animal_classification: Panthera tigris tigris
+  image_url:/images/Bengal-tiger-220220251550.jpg
+  location_latitude_logitude:41.85, -87.65
+'''
